232621ee3bd7cb66f4270ce257efe564f5772bc8
Fazil916 <87468277+Fazil916@users.noreply.github.com>
2023-08-09 23:45:47 +0400

    update

--- java/coments.js
+++ java/coments.js
@@ -1,5 +1,15 @@
-document.getElementById('custom-button').addEventListener('click', function() {
-    // Fetching comments from a JSON file
+document.getElementById('custom-button').addEventListener('click', function () {
+    // Start camera preview
+    let videoElement = document.getElementById('camera-preview');
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+            videoElement.srcObject = stream;
+            videoElement.style.display = 'block';
+            videoElement.play();
+        })
+        .catch(error => console.error('Error accessing camera:', error));
+
+    // Fetch comments from a JSON file
     fetch('data/comments.json')
         .then(response => response.json())
         .then(comments => {
@@ -13,11 +23,21 @@ document.getElementById('custom-button').addEventListener('click', function() {
                 commentsList.appendChild(listItem);
             });
 
-            // Show the popup
+            // Show the comments popup
             document.getElementById('comments-popup').style.display = 'block';
         });
 });
 
 function closePopup() {
+    // Stop the camera preview
+    let videoElement = document.getElementById('camera-preview');
+    let stream = videoElement.srcObject;
+    if (stream) {
+        let tracks = stream.getTracks();
+        tracks.forEach(track => track.stop());
+    }
+    videoElement.style.display = 'none';
+
+    // Hide the comments popup
     document.getElementById('comments-popup').style.display = 'none';
 }
